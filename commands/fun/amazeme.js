2f098df3109e9d511d507749d1afb03142e0a835
Reezzie <76436714+Reezzie@users.noreply.github.com>
2021-05-02 15:07:02 +0400

    Update amazeme.js

--- commands/fun/amazeme.js
+++ commands/fun/amazeme.js
@@ -1,10 +1,3 @@
-/*
- * Antique-gold
- * Copyright (C) 2021 DevZ-Org
- * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
- * For more information, see README.md and LICENSE
-  */
-
 const discord = require('discord.js');
 
 const got = require('got');
